f6810c8105380b0a9a278378fabfdd0661a0d881
Enmah4k <alexmm011@hotmail.com>
2020-02-17 02:41:59 -0400

    Finalizado

---
+++ Ejercicio2/Controllers/ArregloController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ejercicio2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio2.Controllers
+{
+    public class ArregloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Arreglo());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Arreglo arr, string calcular)
+        {
+            if (calcular == "cal")
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    arr.Numeros[i] = int.Parse(Request.Form["num"][i]);                 
+                }
+
+                arr.Mayor = arr.Numeros.Max().ToString();
+                arr.Menor = arr.Numeros.Min().ToString();
+            }
+
+            return View(arr);
+        }
+    }
+}
---
+++ Ejercicio2/Models/Arreglo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Arreglo
+    {
+        private int[] _numeros = new int[10];
+        public int[] Numeros 
+        { 
+            get { return _numeros; }
+            set { }
+        }
+
+        
+
+        public string Mayor { get; set; }
+        public string Menor { get; set; }
+    }
+}
